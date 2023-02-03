4734458434a77d41e84fa2d3b9986b9a281667c4
Artur Danilov <artur.danilov@adesso.de>
2023-02-03 11:29:08 +0100

    Add char shift logic

--- KataRot13/AveCaesar.cs
+++ KataRot13/AveCaesar.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,30 +9,56 @@ namespace KataRot13
 {
     public class AveCaesar
     {
+        public const int verschlüsselungSchicht = 13;
+        public const int indexBuchstabeA = 65;
+        public const int indexBuchstabeZ = 90;
+
         public string Converter(string  str)
         {
             str = str.ToUpper();
-            var ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ ,!";            
-            string temp = "";
+            char[] charArray = str.ToCharArray();
 
-            foreach (var item in str)
-            {
-                int index;
-
-                if (ABC.Contains(item))
+            for (int i = 0; i < charArray.Length; i++)
+            {                
+                if(charArray[i] == ' ' || charArray[i] == ',')
                 {
-                    if (item != ' ' && item != ',' && item != '!')
+                    i++;
+                }
+
+                if (charArray[i] >= indexBuchstabeA && charArray[i] <= indexBuchstabeZ - verschlüsselungSchicht)
+                {
+                    charArray[i] = (char)(charArray[i] + verschlüsselungSchicht);
+                }
+                else
+                {
+                    if (charArray[i] == ' ')
                     {
-                        index = ABC.IndexOf(item);
-                        temp += ABC[index + 13].ToString().ToUpper();
+                        i++;
                     }
-                    else
-                    {
-                        temp += item;
-                    }
-                }               
+                    charArray[i] = (char)(indexBuchstabeA + (verschlüsselungSchicht - (indexBuchstabeZ - charArray[i]) - 1));
+                }
+
             }
-            return temp;
+            return new string(charArray);
+
+            //foreach (var item in str)
+            //{
+            //    int index;
+
+            //    if (ABC.Contains(item))
+            //    {
+            //        if (item != ' ' && item != ',' && item != '!')
+            //        {
+            //            index = ABC.IndexOf(item);
+            //            temp += ABC[index + 13].ToString().ToUpper();
+            //        }
+            //        else
+            //        {
+            //            temp += item;
+            //        }
+            //    }               
+            //}
+            //return temp;
         }
     }
 }
